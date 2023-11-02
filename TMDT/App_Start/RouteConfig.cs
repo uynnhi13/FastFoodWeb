929046fd755020291b470d6a1f42e45969151e49
KimAnh <124763032+KA0512@users.noreply.github.com>
2023-11-02 23:41:02 +0700

    Revert "Details/Index/Login"

--- TMDT/App_Start/RouteConfig.cs
+++ TMDT/App_Start/RouteConfig.cs
@@ -14,18 +14,10 @@ namespace TMDT
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                        name: "KhachHang",
-                        url: "{area}/{controller}/{action=Index}/{id}",
-                        defaults: new { Areas="KhachHang", controller = "Home", action = "Index", id = UrlParameter.Optional }
-                        );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            
         }
     }
 }
