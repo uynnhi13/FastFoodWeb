3e2c47f930086eb00f7d84ae91a613bec6404a9e
lynluvthjk <97950057+lynluvthjk@users.noreply.github.com>
2023-11-02 23:05:00 +0700

    Details/Index/Login

--- TMDT/App_Start/RouteConfig.cs
+++ TMDT/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace TMDT
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                        name: "KhachHang",
+                        url: "{area}/{controller}/{action=Index}/{id}",
+                        defaults: new { Areas="KhachHang", controller = "Home", action = "Index", id = UrlParameter.Optional }
+                        );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
---
+++ TMDT/Areas/KhachHang/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDT.Data;
+
+namespace TMDT.Areas.KhachHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        TMDTThucAnNhanhEntities db = new TMDTThucAnNhanhEntities();
+        // GET: KhachHangVangLai/Home
+        public ActionResult Index()
+        {
+            var lstProduct = db.Products.ToList();
+            return View(lstProduct);
+        }
+
+        public ActionResult LayProductType()
+        {
+            var lstType = db.Categories.ToList();
+            return PartialView(lstType);
+        }
+
+        public ActionResult LocSanPham(int id)
+        {
+            //Lấy sản phẩm theo id Type
+            var lstProductType = db.Products.Where(Product => Product.typeID == id).ToList();
+
+            //Trả về view để render các sản phẩm trên
+            return View("Index", lstProductType);
+        }
+
+        public ActionResult ChiTietSP(int id)
+        {
+            var sp = db.Products.FirstOrDefault(s => s.cateID == id);
+            return View(sp);
+        }
+    }
+}
